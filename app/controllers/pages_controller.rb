967bb4adb3cf8ff91342482551f903fc75440b5c
Taryn <taryn@sugarpush.com.au>
2018-04-04 09:32:13 +1000

    fixed delete function so users can delete their own account

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,18 @@ class PagesController < ApplicationController
   def about
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
    before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
    before_action :correct_user, only: [:edit, :update]
-   before_action :admin_user, only: :destroy
+  
 
   def index
     @users = User.paginate(page: params[:page])
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to WOOFER!"
-      redirect_to @user # it worked!
+      redirect_to edit_user_path(@user) # it worked!
     else
       render :new
     end
